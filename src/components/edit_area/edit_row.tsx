8b8dbbfe3c8498a60242ae267ffc3b5886747a64
ivan.sankov <sii200192@gmail.com>
2024-02-12 21:45:10 +0700

    code style for imports

--- src/components/edit_area/edit_row.tsx
+++ src/components/edit_area/edit_row.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react';
 import { TimelineRow } from '@/interface/action';
 import { CommonProp } from '@/interface/common_prop';
 import { prefix } from '@/utils/deal_class_prefix';
-import { DragLineData } from './drag_lines';
 import { EditAction } from './edit_action';
 import './edit_row.less';
 

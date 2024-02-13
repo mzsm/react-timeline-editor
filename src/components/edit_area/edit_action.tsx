21bfe25f3c00d17e29ee5db64631abef9b0faf20
ivan.sankov <sii200192@gmail.com>
2024-02-13 12:23:06 +0700

    component "EditAction": removed unnecessary state "transform"

--- src/components/edit_area/edit_action.tsx
+++ src/components/edit_area/edit_action.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useLayoutEffect, useState } from 'react';
+import React, { FC, useLayoutEffect, useMemo, useState } from 'react';
 import { TimelineAction, TimelineRow } from '@/interface/action';
 import { CommonProp } from '@/interface/common_prop';
 import { prefix } from '@/utils/deal_class_prefix';
@@ -22,39 +22,18 @@ export const EditAction: FC<EditActionProps> = ({
 }) => {
   const { end, start, selected } = action;
 
-  // 初始化动作坐标数据
-  const [transform, setTransform] = useState(() => {
-    return parserTimeToTransform({ start, end }, { startLeft, scale, scaleWidth });
-  });
-
-  useLayoutEffect(() => {
-    setTransform(parserTimeToTransform({ start, end }, { startLeft, scale, scaleWidth }));
-  }, [end, start, startLeft, scaleWidth, scale]);
+  const transform = useMemo(() => parserTimeToTransform({ start, end }, { startLeft, scale, scaleWidth }), [end, start, startLeft, scaleWidth, scale]);
 
   // 动作的名称
   const classNames = ['action'];
   if (selected) classNames.push('action-selected');
-
-
-  const nowAction = {
-    ...action,
-    ...parserTransformToTime({ left: transform.left, width: transform.width }, { startLeft, scaleWidth, scale }),
-  };
-
-  const nowRow: TimelineRow = {
-    ...row,
-    actions: [...row.actions],
-  };
-  if (row.actions.includes(action)) {
-    nowRow.actions[row.actions.indexOf(action)] = nowAction;
-  }
 
   return (
     <div
       className={prefix((classNames || []).join(' '))}
       style={{ height: rowHeight, width: transform.width, left: transform.left }}
     >
-      {getActionRender && getActionRender(nowAction, nowRow)}
+      {getActionRender && getActionRender(action, row)}
     </div>
   );
 };
